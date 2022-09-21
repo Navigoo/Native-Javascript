b13b41a3c2be4efbc8a60532d80f49c9650645c1
Navigoo <81353827+Navigoo@users.noreply.github.com>
2022-09-21 22:07:15 +0200

    Index

---
+++ index.js
@@ -0,0 +1,36 @@
+/*Välkommen till Native Javascript Kursen 2022!
+I denna fil kommer all JS kod att publiceras allt efterssom vi går igenom varje 
+sektion på lektionerna.
+Kommentarer och termer kommer att genomföras på engelska för att ni lättare sen kan 
+arbeta som i branschen.
+De kod-projekt som kommer göras under kursens gång läggs på annan sida.
+Övertygad om att ni kommer klara det här jättebra och önskar alla lycka till!!*/
+
+
+/* Variabler, Konstanter och datatyper */
+/* 1. Variables, 2. Contants and 3. data types */
+
+/* 1. Variables */
+
+
+
+/* 2. Constants */
+
+
+
+/* 3. Data Types */
+
+
+
+/* Operatorer för aritmetik (och strängsammanslagning) */
+/* Operators and String Concats */
+
+/* 1. Operators */
+
+
+
+/* 2. String Con */
+
+
+
+/* 3. Data Types */
